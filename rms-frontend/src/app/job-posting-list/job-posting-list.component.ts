af1b493151ef6cc538ae51cae826284ee8ef3fb5
BenJ1337 <email@benjamin-hacker.de>
2025-04-18 12:10:22 +0200

    update

--- rms-frontend/src/app/job-posting-list/job-posting-list.component.ts
+++ rms-frontend/src/app/job-posting-list/job-posting-list.component.ts
@@ -3,16 +3,19 @@ import { JobPostingServiceService } from '../service/job-posting-service.service
 import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { AsyncPipe, CommonModule } from '@angular/common';
 import { IJobPosting } from '../domain/job_posting';
+import { MatSnackBar, MatSnackBarModule, MatSnackBarVerticalPosition } from '@angular/material/snack-bar';
 import { Observable, map } from 'rxjs';
 
 @Component({
   selector: 'app-job-posting-list',
   templateUrl: './job-posting-list.component.html',
   styleUrl: './job-posting-list.component.css',
-  imports: [MatTableModule, AsyncPipe, CommonModule]
+  imports: [MatTableModule, AsyncPipe, CommonModule, MatSnackBarModule]
 })
 export class JobPostingListComponent {
   private jobPostingService = inject(JobPostingServiceService);
+  private snackbar = inject(MatSnackBar);
+
 
   columnsDisplay: string[] = ['title', 'description'];
   dataSource: MatTableDataSource<IJobPosting> = new MatTableDataSource<IJobPosting>();
@@ -21,6 +24,7 @@ export class JobPostingListComponent {
     this.jobPostingService.getJobPostings().subscribe(jobpostings => {
       this.dataSource.data = jobpostings;
     });
+    this.snackbar.open('Hello World', '', { duration: 1000, verticalPosition: 'top' });
   }
 
 }
